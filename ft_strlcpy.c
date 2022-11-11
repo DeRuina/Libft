88e71b3c9b2ad11f414de95783eb239d1317ff07
Dean Ruina <druina@c2r5p9.hive.fi>
2022-11-11 13:41:33 +0200

    passed project

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,12 +6,11 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:13:32 by druina            #+#    #+#             */
-/*   Updated: 2022/11/09 17:41:22 by druina           ###   ########.fr       */
+/*   Updated: 2022/11/11 13:40:15 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 {
@@ -22,7 +21,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 		return (ft_strlen((char *)src));
 	if (destsize > 0)
 	{
-		while (*(src + move) && move != (int)destsize -1)
+		while (*(src + move) && move != (int)destsize - 1)
 		{
 			if (move == (int)destsize)
 			{
